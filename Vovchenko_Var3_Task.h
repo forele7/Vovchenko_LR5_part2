41b2b94f5afc5ce7cc9190793e9b2151383b9fba
forele7 <mar_vov2008@mail.ru>
2025-04-19 17:10:36 +0300

    Vovchenko_Var3 Modified

--- Vovchenko_Var3_Task.h
+++ Vovchenko_Var3_Task.h
@@ -59,4 +59,52 @@ function<void()> Enter_Number(double& varLink, string label)
     };
 }
 
+//число А
+double a;
+void setA(double a);
+double getA() {return a;};
+
+//число В
+double b;
+void setB(double b);
+double getB() {return b;};
+
+struct Menu1
+{
+    string title;
+    function<void()> action;
+};
+
+//задание 1
+void ShowAnum()
+{
+    Enter_Number(a, "Enter number A: ")();
+    cout << a << endl;
+}
+
+//задание 2
+void ShowBnum()
+{
+    Enter_Number(b, "Enter number B: ")();
+    cout << b << endl;
+}
+
+// //задание 3
+void Operation1()
+{
+    Enter_Number(a, "Enter number A: ")();
+    Enter_Number(b, "Enter number B: ")();
+    cout << "A*B = " << a*b << endl;
+}
+
+// // задание 4
+void Operation2()
+{
+    Enter_Number(a, "Enter number A: ")();
+    Enter_Number(b, "Enter number B: ")();
+    if (b == 0){cout << "На ноль делить нельзя!";}
+    else {cout << "A/B = " << a/b << endl;}
+}
+
+
 #endif
