f3d9c8bb646db9d604e890d5cb4e66990164e135
forele7 <mar_vov2008@mail.ru>
2025-04-23 15:58:28 +0300

    Vovchenko_Var3 Modified 2

--- Vovchenko_Var3_Main.cpp
+++ Vovchenko_Var3_Main.cpp
@@ -1,48 +1,94 @@
 #include "Vovchenko_Var3_Task.h"
 
+using namespace cute;
 using namespace std;
 
 int main()
 {
-    map<int, Menu1> menu =
-    {
-        {1, {"Вывести A", ShowAnum}},
-        {2, {"Вывести B", ShowBnum}},
-        {3, {"Выполнить операцию “*”", Operation1}},
-        {4, {"Выполнить операцию “/”", Operation2}},
-    };
+    suite s;
 
-    double choice = 0;
+    s.push_back(CUTE(ShowNumA1));
+    s.push_back(CUTE(ShowNumA2));
+    s.push_back(CUTE(ShowNumA3));
 
-    cout << "Menu: " << endl;
-    for (const auto& item : menu)
-    {
-        cout << "Task: " <<item.first << ". " << item.second.title << endl;
-    }
+    s.push_back(CUTE(ShowNumB1));
+    s.push_back(CUTE(ShowNumB2));
+    s.push_back(CUTE(ShowNumB3));
 
-    cout << "0. Quit." << endl;
+    s.push_back(CUTE(test1Operation1));
+    s.push_back(CUTE(test2Operation1));
 
-    while (true)
-    {
-    Enter_Number(choice, "Enter Number of task: ")();
-    if (choice == 0)
-    {
-        cout << endl;
-        break;
-    }
+    s.push_back(CUTE(test1Operation2));
+    s.push_back(CUTE(test2Operation2));
 
-    cout << endl;
-
-    if (menu.find(choice) != menu.end())
-    {
-        menu[choice].action();
-    }
-    else
-    {
-        cout << "Invalid input.";
-    }
-
-    cout << endl << endl;
-    }
-    return 0;
+    ide_listener<> listener;
+    makeRunner(listener)(s, "Test");
 }
+
+// struct Menu1
+// {
+//     string title;
+//     function<void()> action;
+// };
+
+// int main()
+// {
+//     suite s;
+//     // s.push_back(CUTE(ShowNumA1));
+
+//     map<int, Menu1> menu =
+//     {
+//         // {1, {"Тест 1: Вывести A", [&s]() { s.push_back(CUTE(ShowNumA1)); }}},
+//         // {2, {"Тест 2: Вывести A", ShowNumA2}},
+//         // {3, {"Тест 3: Вывести A", ShowNumA3}},
+//         // {4, {"Тест 4: Вывести A", ShowNumA4}},
+
+//         // {5, {"Тест 1: Вывести B", ShowNumB1}},
+//         // {6, {"Тест 2: Вывести B", ShowNumB2}},
+//         // {7, {"Тест 3: Вывести B", ShowNumB3}},
+//         // {8, {"Тест 4: Вывести B", ShowNumB4}},
+
+//         {9, {"Тест 1: Выполнить операцию “*”", CUTE(test1Operation1)}},
+//         //{10, {"Тест 2: Выполнить операцию “*”", Operation1Test2}},
+
+//         {11, {"Выполнить операцию “/”", test1Operation2}},
+//         // {12, {"Выполнить операцию “/”", Operation2Test2}},
+
+//     };
+//     ide_listener<> listener;
+//     makeRunner(listener)(s, "Test");
+
+//     double choice = 0;
+
+//     cout << "Menu: " << endl;
+//     for (const auto& item : menu)
+//     {
+//         cout << "Task: " <<item.first << ". " << item.second.title << endl;
+//     }
+
+//     cout << "0. Quit." << endl;
+
+//     while (true)
+//     {
+//     Enter_Number(choice, "Enter Number of task: ")();
+//     if (choice == 0)
+//     {
+//         cout << endl;
+//         break;
+//     }
+
+//     cout << endl;
+
+//     if (menu.find(choice) != menu.end())
+//     {
+//         menu[choice].action();
+//     }
+//     else
+//     {
+//         cout << "Invalid input.";
+//     }
+
+//     cout << endl << endl;
+//     }
+//     return 0;
+// }
--- Vovchenko_Var3_Task.h
+++ Vovchenko_Var3_Task.h
@@ -1,6 +1,10 @@
 #ifndef _VOVCHENKO_VAR3TASK_H_
 #define _VOVCHENKO_VAR3TASK_H_
 
+#include "CUTE/cute/cute.h"
+#include "CUTE/cute/ide_listener.h"
+#include "CUTE/cute/cute_runner.h"
+
 #include <iostream>
 #include <vector>
 #include <sstream>
@@ -59,51 +63,114 @@ function<void()> Enter_Number(double& varLink, string label)
     };
 }
 
-//число А
-double a;
-void setA(double a);
-double getA() {return a;};
-
-//число В
-double b;
-void setB(double b);
-double getB() {return b;};
-
-struct Menu1
+//тест А1: отсев буквенных значений
+void ShowNumA1()
 {
-    string title;
-    function<void()> action;
-};
-
-//задание 1
-void ShowAnum()
-{
-    Enter_Number(a, "Enter number A: ")();
-    cout << a << endl;
+    string A = "ww";
+    bool expected = false;
+    bool actual = Input(A);
+    ASSERT_EQUAL(expected, actual);
 }
 
-//задание 2
-void ShowBnum()
+// тест А2: отсев цифро-буквенных значений
+void ShowNumA2()
 {
-    Enter_Number(b, "Enter number B: ")();
-    cout << b << endl;
+    string A = "7e";
+    bool expected = false;
+    bool actual = Input(A);
+    ASSERT_EQUAL(expected, actual);
 }
 
-// //задание 3
-void Operation1()
+// тест А3: отсев пустых значений
+void ShowNumA3()
 {
-    Enter_Number(a, "Enter number A: ")();
-    Enter_Number(b, "Enter number B: ")();
-    cout << "A*B = " << a*b << endl;
+    string A = "";
+    bool expected = false;
+    bool actual = Input(A);
+    ASSERT_EQUAL(expected, actual);
 }
 
-// // задание 4
-void Operation2()
+// тест В1: отсев пустых значений
+void ShowNumB1()
 {
-    Enter_Number(a, "Enter number A: ")();
-    Enter_Number(b, "Enter number B: ")();
-    if (b == 0){cout << "На ноль делить нельзя!";}
-    else {cout << "A/B = " << a/b << endl;}
+    string B = "";
+    bool expected = false;
+    bool actual = Input(B);
+    ASSERT_EQUAL(expected, actual);
+}
+
+// тест В2: отсев буквенных значений
+void ShowNumB2()
+{
+    string B = "-3";
+    bool expected = false;
+    bool actual = Input(B);
+    ASSERT_EQUAL(expected, actual);
+}
+
+// тест В3: отсев цифро-буквенных значений
+void ShowNumB3()
+{
+    string B = "e9";
+    bool expected = false;
+    bool actual = Input(B);
+    ASSERT_EQUAL(expected, actual);
+}
+
+// задание 3
+int testCalc1(double A, double B)
+{
+    return A * B;
+}
+
+// тест 1: умножение
+void test1Operation1()
+{
+    double A = 9;
+    double B = 4;
+
+    double expected = 36;
+    double actual = testCalc1(A, B);
+    ASSERT_EQUAL (expected, actual);
+}
+
+// тест 2: умножение
+void test2Operation1()
+{
+    double A = 6;
+    double B = 7;
+
+    double expected = 50;
+    double actual = testCalc1(A, B);
+    ASSERT_EQUAL (expected, actual);
+}
+
+//задание 4
+int testCalc2(double A, double B)
+{
+    return A / B;
+}
+
+// тест 1: деление
+void test1Operation2()
+{
+    double A = 8;
+    double B = 2;
+
+    double expected = 4;
+    double actual = testCalc2(A, B);
+    ASSERT_EQUAL (expected, actual);
+}
+
+// тест 2: деление
+void test2Operation2()
+{
+    double A = 40;
+    double B = 10;
+
+    double expected = 6;
+    double actual = testCalc2(A, B);
+    ASSERT_EQUAL (expected, actual);
 }
 
 #endif
